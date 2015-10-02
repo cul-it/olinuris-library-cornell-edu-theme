e3071049b981c9967ba766de75f0b530037b6a61
Melissa Wallace <mhk33@cornell.edu>
2015-10-02 13:57:59 -0400

    new brand css

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -134,7 +134,7 @@
         <i class="fa fa-search"></i>
       </button>
       <div class="cu-insignia">
-        <a href="http://www.cornell.edu"><img src="/sites/all/themes/aandc/img/CU-Insignia-Black-120.png" alt="Cornell University" class="img-responsive hidden-xs"></a>
+        <a href="http://www.cornell.edu"><img src="/sites/all/themes/aandc/img/CU-Insignia-Red-120.png" alt="Cornell University" class="img-responsive hidden-xs"></a>
       </div>
       <div class="library-brand">
         <a class="cul-brand" href="http://www.library.cornell.edu">Cornell University Library</a>
